ee9492b69597d2a3512f7c1579ed940b40e70d06
Gordon Doskas <gordon2012@users.noreply.github.com>
2017-10-01 06:36:52 -0700

    Fix delete article
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -118,7 +118,7 @@ class ArticlesController extends AppController
         $this->request->allowMethod(['post', 'delete']);
 
         $article = $this->Articles->get($id);
-        if($this->Articles->get($id)) {
+        if($this->Articles->delete($article)) {
             $this->Flash->success(__('The Article with id: {0} has been deleted.', h($id)));
             return $this->redirect(['action'=>'index']);
         }
